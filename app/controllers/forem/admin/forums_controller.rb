dd743c72edf99cee9f0123d30d004b2d584e250b
Yuva <yuva@codemancers.com>
2013-04-11 16:06:25 +0530

    admin should be checked for permissions while creating forums

--- app/controllers/forem/admin/forums_controller.rb
+++ app/controllers/forem/admin/forums_controller.rb
@@ -4,35 +4,48 @@ module Forem
       before_filter :find_forum, :only => [:edit, :update, :destroy]
 
       def index
-        @forums = Forem::Forum.all
+        @forums = Forem::Forum.accessible_by(current_ability)
       end
 
       def new
+        @categories = Forem::Category.accessible_by(current_ability)
         @forum = Forem::Forum.new
       end
 
       def create
         @forum = Forem::Forum.new(forum_params)
+        authorize!(:create, @forum)
+
         if @forum.save
           flash[:notice] = t("forem.admin.forum.created")
           redirect_to admin_forums_path
         else
+          @categories = Forem::Category.accessible_by(current_ability)
           flash.now.alert = t("forem.admin.forum.not_created")
           render :action => "new"
         end
       end
 
+      def edit
+        @categories = Forem::Category.accessible_by(current_ability)
+      end
+
       def update
+        authorize!(:update, @forum)
+
         if @forum.update_attributes(forum_params)
           flash[:notice] = t("forem.admin.forum.updated")
           redirect_to admin_forums_path
         else
+          @categories = Forem::Category.accessible_by(current_ability)
           flash.now.alert = t("forem.admin.forum.not_updated")
           render :action => "edit"
         end
       end
 
       def destroy
+        authorize!(:destroy, @forum)
+
         @forum.destroy
         flash[:notice] = t("forem.admin.forum.deleted")
         redirect_to admin_forums_path
